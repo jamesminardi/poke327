7560844b0952650701fec9a05a8511c548d43043
jamesminardi <jamesminardip@gmail.com>
2022-02-02 16:11:29 -0600

    Add initialization and print methods

--- main.c
+++ main.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
+#define COLS 80
+#define ROWS 21
+
+
+void world_init(char world[ROWS][COLS]);
+void world_print(char world[ROWS][COLS]);
 
 int main(int argc, char *argv[]) {
 
+  char world[ROWS][COLS];
   printf("Hello, World!\n");
+  world_init(world);
+  world_print(world);
 
   return 0;
+  
+}
 
+void world_init(char world[ROWS][COLS]) {
+
+  unsigned int i;
+  unsigned int j;
+  for (i = 0; i < ROWS; i++) {
+    for (j = 0; j < COLS; j++) {
+      
+      world[i][j] = '.';
+      
+    }
+  }
+  
+}
+
+void world_print(char world[ROWS][COLS]) {
+  unsigned int i;
+  unsigned int j;
+  
+    for (i = 0; i < 21; i++) {
+      for (j = 0; j < 80; j++) {
+	
+	printf("%c", world[i][j]);
+      }
+      printf("\n");
+    }
 }
