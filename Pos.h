20ab9c284ffbccd793f3f9f5ad0b58547f2b8e70
jamesminardi <jamesminardip@gmail.com>
2022-04-23 04:29:12 -0500

    Add relative and manhattan method to position class

--- Pos.h
+++ Pos.h
@@ -3,21 +3,42 @@
 
 #include <cmath>
 
-typedef struct pos {
+typedef struct Pos {
 	int x;
 	int y;
-	bool operator==(const pos& a) const
+	bool operator==(const Pos& a) const
 	{
 		return (x == a.x && y == a.y);
 	}
-	bool operator!=(const pos& a) const
+	bool operator!=(const Pos& a) const
 	{
 		return (x != a.x || y != a.y);
+	}
+	inline int manhattan(Pos p) {
+		return abs(x - p.x) + abs(y - p.y);
+	}
+	/*
+	 * returns relative position from caller object to parameter p
+	 */
+	inline Pos relative(Pos p) {
+		Pos relativePos;
+		relativePos.x = x - p.x;
+		relativePos.y = y - p.y;
+		return relativePos;
 	}
 } pos_t;
 
 inline int manhattan(pos_t p1, pos_t p2) {
 	return abs(p1.x - p2.x) + abs(p1.y - p2.y);
 }
+
+//static std::string relativePosString(int x, int y) {
+//	pos_t relPos;
+//	relPos.x = x - world.pc->pos.x;
+//	relPos.y = y - world.pc->pos.y;
+//	std::string str = "(" + std::to_string(relPos.x) + ", " + std::to_string(relPos.y) + ")";
+//	return str;
+//
+//}
 
 #endif //POKE327_POS_H
